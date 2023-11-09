ced99e8e10028bc73fdb8b0c6a05fedcbb45e1ed
whoamikunal <kunalpundir111@gmail.com>
2023-11-09 23:28:26 +0530

    project setup

--- src/main/java/com/demo/todo/TodoApplication.java
+++ src/main/java/com/demo/todo/TodoApplication.java
@@ -1,12 +1,17 @@
 package com.demo.todo;
 
+import lombok.extern.slf4j.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class TodoApplication {
 
 	public static void main(String[] args) {
+		log.info("Starting application....");
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
